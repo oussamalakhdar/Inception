2cc66c2981f9a5f5c8c18a4f0e91e1155ada7aa0
Oussama Lakhdar <lakhdaroussama99@gmail.com>
2022-12-12 20:24:41 +0100

    ola

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -3,7 +3,14 @@
 wp core config --allow-root --dbhost="mariadb" --dbname="wordpress" --dbuser="olakhdar" --dbpass="oussama@1"
 chmod 600 wp-config.php
 chown -R www-data *
+
+wp config set --allow-root 'WP_REDIS_HOST' 'redis';
+wp config set --allow-root 'WP_REDIS_PORT' 6379;
+
 wp core install --allow-root --url="localhost" --title="olakhdar" --admin_name="olakhdar" --admin_password="oussama@1" --admin_email="olakhdar@student.1337.ma"
-wp user create --allow-root oussama lakhdaroussama99@gmail.com --user_pass=oussama@2 --role=editor
+wp user create --allow-root oussama lakhdaroussama99@gmail.com --user_pass=oussama@2 --role=author
+
+wp plugin install --allow-root redis-cache --activate
+wp redis enable --allow-root
 
 exec php-fpm7.3 -F -R

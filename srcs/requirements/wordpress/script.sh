81cb13b788433386eae0cfc1aaa2d473c8bfbd96
Oussama Lakhdar <lakhdaroussama99@gmail.com>
2022-12-12 12:03:07 +0100

    ince

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -3,6 +3,7 @@
 wp core config --allow-root --dbhost="mariadb" --dbname="wordpress" --dbuser="olakhdar" --dbpass="oussama@1"
 chmod 600 wp-config.php
 chown -R www-data *
-wp core install --allow-root --url="localhost" --title="oussama" --admin_name="olakhdar" --admin_password="oussama@1" --admin_email="olakhdar@student.1337.ma"
+wp core install --allow-root --url="localhost" --title="olakhdar" --admin_name="olakhdar" --admin_password="oussama@1" --admin_email="olakhdar@student.1337.ma"
+wp user create --allow-root oussama lakhdaroussama99@gmail.com --user_pass=oussama@2 --role=editor
 
 exec php-fpm7.3 -F -R

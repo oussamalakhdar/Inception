b0cae12a8897c3dbb27473ba2b3b3adb3830f24d
olakhdar <lakhdaroussama99@gmail.com>
2023-01-10 16:48:00 +0100

    done all

--- srcs/requirements/bonus/ftp-server/tools/script.sh
+++ srcs/requirements/bonus/ftp-server/tools/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos '' oussama 
+adduser --disabled-password --gecos '' oussama
 
 echo "oussama" | tee -a /etc/vsftpd.userlist
-echo "oussama:1234" | chpasswd 
+echo "oussama:1234" | chpasswd
 mkdir -p /home/oussama/empty
 chown oussama:oussama /home/oussama/wordpress
 chown oussama:oussama /home/oussama/empty
